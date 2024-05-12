723e3973f05ea0dbdaa67d0e1f9d79f154d7f0e2
manonmars69 <23418875+manonmars69@users.noreply.github.com>
2024-05-12 11:13:47 +0200

    Lorem_ipsum.txt added to books,.gitignore excepetions modified, main.py modified.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 def main ():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    
+    try:
+        book_path = input("Please enter book path: ")
+        b_path = "books/frankenstein.txt"
 
-main()
+        def read_book (book_path): 
+            with open(book_path) as f:
+                file_contents = f.read()
+                print(file_contents)
+        return read_book(book_path)
+    
+    except FileNotFoundError:
+        print("Please enter a correct path - f.e.: books/lorem_ipsum.txt") 
+    
+    
+    #def word_counter():
+            #words = 0
+            #for i in file_contents:
+                #words += i
+            #print(words)
+
+#review ch.13 & 14
+
+main ()
+
